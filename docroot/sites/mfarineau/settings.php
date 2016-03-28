e18baeb9c477199a1efaf81b5472a500a7dabffa
Matt Farineau <matt.farineau@acquia.com>
2016-03-28 14:24:34 -0400

    fix settings.php for multisite

--- docroot/sites/mfarineau/settings.php
+++ docroot/sites/mfarineau/settings.php
@@ -723,9 +723,6 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 // database in each site environment (Dev, Stage, or Prod). To use this
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-  require('/var/www/site-php/eemfarineau/eemfarineau-settings.inc');
-}
 
 // <DDSETTINGS>
 // Please don't edit anything between <DDSETTINGS> tags.
