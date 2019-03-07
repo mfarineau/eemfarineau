3d6c823f1c052e7e23a2e0cc95967c19c1946e13
Matt Farineau <mfarineau@gmail.com>
2019-03-07 11:28:55 -0500

    update settings for dashboard

--- docroot/sites/dashboard/settings.php
+++ docroot/sites/dashboard/settings.php
@@ -730,7 +730,7 @@ else {
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
 if (file_exists('/var/www/site-php')) {
-  require('/var/www/site-php/eemfarineau/eemfarineau-settings.inc');
+  require '/var/www/site-php/eemfarineau/supportdash-settings.inc';
 }
 
 // <DDSETTINGS>
